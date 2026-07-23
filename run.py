e4e7fb874bd371c00dd016ac6e285f5ae3d19d7a
Anirudh Cherukuri <vamshicherukuri41@gmail.com>
2026-07-23 22:07:41 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
+import os
 import uvicorn
-from backend.config import HOST, PORT
+
+HOST = "0.0.0.0"
+PORT = int(os.environ.get("PORT", 8000))
 
 if __name__ == "__main__":
     print("=========================================================")
@@ -9,4 +12,10 @@ if __name__ == "__main__":
     print(f" Swagger Docs: http://localhost:{PORT}/docs")
     print(f" Health Check: http://localhost:{PORT}/api/v1/health")
     print("=========================================================")
-    uvicorn.run("backend.main:app", host=HOST, port=PORT, reload=True)
+
+    uvicorn.run(
+        "backend.main:app",
+        host=HOST,
+        port=PORT,
+        reload=False
+    )
